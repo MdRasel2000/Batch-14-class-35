1730ea2e6a06e3535fc650fc1b832cb12081ebed
MdRasel2000 <ctgmdraselislam@gmail.com>
2025-02-01 17:44:20 +0600

    PHP Function & CRUD Intro

---
+++ index.php
@@ -0,0 +1,61 @@
+<?php 
+
+// function sum($x, $y){
+
+
+//      $result = $x + $y;
+
+
+//      return $result;
+
+
+// }
+
+// function multiply ($number){
+
+//          $result = sum(x:800, y:200);
+
+//          $multiply = $result*$number;
+
+//          return $multiply;
+        
+
+// }
+
+//  function division ($x, $y){
+
+//          $result= $x/$y;
+
+//          return $result;
+
+
+//  }
+
+
+
+  
+//  echo sum(x:30, y: 80). "<br>";
+
+
+// $result2 = sum(x: 60, y: 70) + 30 ;
+
+// echo $result2. "<br>";
+
+// echo sum(x:40,y:60);
+
+
+
+// /
+
+
+// echo division(x:sum(x:60 ,y:40), y:2);
+
+
+
+
+
+
+
+
+
+?>
